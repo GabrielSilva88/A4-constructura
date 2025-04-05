e02b7408e65815cf5ae44f66fd327ab1f43d385f
Gabri <gabrielmg.silva@outlook.com>
2025-04-05 14:29:18 -0300

    Add components, module e routing

---
+++ src/app/lancamentos/lancamentos.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { LancamentosRoutingModule } from './lancamentos-routing.module';
+
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    CommonModule,
+    LancamentosRoutingModule
+  ]
+})
+export class LancamentosModule { }
