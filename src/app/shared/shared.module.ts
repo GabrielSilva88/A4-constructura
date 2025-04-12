8a4d2e2c1c51f850f6905de66166b946d7b31d40
Gabri <gabrielmg.silva@outlook.com>
2025-04-12 12:14:58 -0300

    Atualização projetos dia 12-04-25

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DaoService } from './services/dao.service';
 
-
+export const declrationss = [
+  //  DinheiroDirective,
+  //    MaiusculoDirective,
+  //    MenuTypeEnum,
+  //   OperacaoTypeEnum
+  ]
 
 @NgModule({
-  declarations: [],
+   declarations: [
+    declrationss
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    declrationss
+  ],
+  providers:[
+    DaoService
   ]
 })
 export class SharedModule { }
